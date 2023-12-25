326c381704a7033382b78e15776c3d0c618f52b7
BB3M <beam0624038791@gmail.com>
2023-12-26 01:10:28 +0700

    feat: add better reference page.

--- src/referencePage.jsx
+++ src/referencePage.jsx
@@ -1,19 +1,78 @@
-export const ReferencePage = () => {  
-    return (
-        <div className="flex flex-row justify-center min-h-screen" >
-          <div className="flex flex-col w-full bg-[url('src/assets/bg.jpg')] bg-fixed bg-no-repeat bg-cover items-center gap-3">
-            <div className="my-10 md:my-16"></div>
-            <div className=" flex justify-center w-2/3 md:w-1/2 lg:w-1/3">
-                 <img src="src/assets/Opink.png" className="rounded-xl"></img>
-            </div>
+export const ReferencePage = () => {
+  return (
+    <div className="flex flex-row justify-center min-h-screen" >
+      <div className="flex flex-col w-full bg-[url('src/assets/bg.jpg')] bg-fixed bg-no-repeat bg-cover gap-3 ">
+        <div className="my-10 md:my-16"></div>
 
-            <div className="flex h-fit bg-white bg-opacity-30 rounded-lg shadow-md backdrop-blur-sm max-w-4xl p-3 w-11/12 md:w-5/6 lg:w-4/6">
-                <p className="text-lg md:text-2xl lg:text-4xl text-white font-bold">ประวัติการศึกษา/ผลงาน</p>
+        <div className="flex shadow-md">
+
+          <img id="opink" className="flex w-2/3 md:w-1/2 lg:w-1/5 ml-20" src="src/assets/Opink.png"></img>
+
+          <div className="flex w-2/3 md:w-1/2 lg:w-2/5 flex-col ">
+            <div className="flex justify-center bg-white bg-opacity-50 shadow-md rounded-tr backdrop-blur-sm max-w-6xl p-3 w-11/12 md:w-5/6 lg:w-10/11">
+              <div className="text-lg md:text-2xl lg:text-4xl text-white font-bold">ประวัติการศึกษา/ผลงาน</div>
             </div>
-            <div className="w-full flex justify-center mb-1 mt-2">
-              <hr className="w-4/6 border-y-2 opacity-60"></hr>
+            <div className="flex justify-center bg-white bg-opacity-30 shadow-md rounded-br backdrop-blur-sm max-w-5xl max-h-4xl p-4 w-11/12 md:w-5/6 md:h-2/3 lg:w-10/11 lg:h-4/5">
+              <div className="text-md md:text-xl lg:text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum ullam quod ut est repellendus dolore numquam soluta ipsum ducimus quo vitae dolores perspiciatis, ea dignissimos ad repellat libero saepe.</div>
             </div>
-          </div >
-        </div >
-      );
+          </div>
+          {/* <div className="w-full flex justify-center mb-1 mt-2">
+            <hr className="w-4/6 border-y-2 opacity-60"></hr>
+          </div> */}
+        </div>
+        
+        <div className="flex my-5 shadow-md">
+
+          <img className="flex object-cover w-2/3 md:w-1/2 lg:w-1/5 ml-20" src="src/assets/kpbo.jpg"></img>
+
+          <div className="flex w-2/3 md:w-1/2 lg:w-2/5 flex-col ">
+            <div className="flex justify-center bg-white bg-opacity-50 shadow-md rounded-tr backdrop-blur-sm max-w-6xl p-3 w-11/12 md:w-5/6 lg:w-10/11">
+              <div className="text-lg md:text-2xl lg:text-4xl text-white font-bold">ประวัติการศึกษา/ผลงาน</div>
+            </div>
+            <div className="flex justify-center bg-white bg-opacity-30 shadow-md rounded-br backdrop-blur-sm max-w-5xl max-h-4xl p-4 w-11/12 md:w-5/6 md:h-2/3 lg:w-10/11 lg:h-4/5">
+              <div className="text-md md:text-xl lg:text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum ullam quod ut est repellendus dolore numquam soluta ipsum ducimus quo vitae dolores perspiciatis, ea dignissimos ad repellat libero saepe.</div>
+            </div>
+          </div>
+          {/* <div className="w-full flex justify-center mb-1 mt-2">
+            <hr className="w-4/6 border-y-2 opacity-60"></hr>
+          </div> */}
+        </div>
+
+        <div className="flex my-5 shadow-md">
+
+          <img className="flex object-cover w-2/3 md:w-1/2 lg:w-1/5 ml-20" src="src/assets/beam.jpg"></img>
+
+          <div className="flex w-2/3 md:w-1/2 lg:w-2/5 flex-col ">
+            <div className="flex justify-center bg-white bg-opacity-50 shadow-md rounded-tr backdrop-blur-sm max-w-6xl p-3 w-11/12 md:w-5/6 lg:w-10/11">
+              <div className="text-lg md:text-2xl lg:text-4xl text-white font-bold">ประวัติการศึกษา/ผลงาน</div>
+            </div>
+            <div className="flex justify-center bg-white bg-opacity-30 shadow-md rounded-br backdrop-blur-sm max-w-5xl max-h-4xl p-4 w-11/12 md:w-5/6 md:h-2/3 lg:w-10/11 lg:h-4/5">
+              <div className="text-md md:text-xl lg:text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum ullam quod ut est repellendus dolore numquam soluta ipsum ducimus quo vitae dolores perspiciatis, ea dignissimos ad repellat libero saepe.</div>
+            </div>
+          </div>
+          {/* <div className="w-full flex justify-center mb-1 mt-2">
+            <hr className="w-4/6 border-y-2 opacity-60"></hr>
+          </div> */}
+        </div>
+        
+        <div className="flex my-5 shadow-md">
+
+          <img className="flex object-cover w-2/3 md:w-1/2 lg:w-1/5 ml-20" src="src/assets/Patty.jpg"></img>
+
+          <div className="flex w-2/3 md:w-1/2 lg:w-2/5 flex-col ">
+            <div className="flex justify-center bg-white bg-opacity-50 shadow-md rounded-tr backdrop-blur-sm max-w-6xl p-3 w-11/12 md:w-5/6 lg:w-10/11">
+              <div className="text-lg md:text-2xl lg:text-4xl text-white font-bold">ประวัติการศึกษา/ผลงาน</div>
+            </div>
+            <div className="flex justify-center bg-white bg-opacity-30 shadow-md rounded-br backdrop-blur-sm max-w-5xl max-h-4xl p-4 w-11/12 md:w-5/6 md:h-2/3 lg:w-10/11 lg:h-4/5">
+              <div className="text-md md:text-xl lg:text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum ullam quod ut est repellendus dolore numquam soluta ipsum ducimus quo vitae dolores perspiciatis, ea dignissimos ad repellat libero saepe.</div>
+            </div>
+          </div>
+          {/* <div className="w-full flex justify-center mb-1 mt-2">
+            <hr className="w-4/6 border-y-2 opacity-60"></hr>
+          </div> */}
+        </div>
+
+      </div >
+    </div >
+  );
 }
